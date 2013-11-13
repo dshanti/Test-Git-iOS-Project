2a2afeab398f949e58e31074349f091fabbed79a
delima <delimaxw@gmail.com>
2013-11-14 10:30:49 +1100

    added a test class

---
+++ Git test/DSTest.h
@@ -0,0 +1,13 @@
+//
+//  DSTest.h
+//  Git test
+//
+//  Created by Delima Shanti on 14/11/2013.
+//  Copyright (c) 2013 Delima Shanti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DSTest : NSObject
+
+@end
---
+++ Git test/DSTest.m
@@ -0,0 +1,13 @@
+//
+//  DSTest.m
+//  Git test
+//
+//  Created by Delima Shanti on 14/11/2013.
+//  Copyright (c) 2013 Delima Shanti. All rights reserved.
+//
+
+#import "DSTest.h"
+
+@implementation DSTest
+
+@end
